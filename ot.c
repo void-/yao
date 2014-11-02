ee098a43722dc1f9404c00b3953811a7448931f9
void- <mr.alex.guy@gmail.com>
2014-11-01 19:37:17 -0700

    ot: Documented oblivious transfer functions.

---
+++ ot.c
@@ -0,0 +1,41 @@
+#include "ot.h"
+
+/**
+ *  @brief send one of two secrets via an oblivious transfer given a socket.
+ *
+ *  Important parameter notes:
+ *  <p>
+ *  - The ordering of the secrets may be important.
+ *  - \p secret0 and \p secret1 must both be \p size bytes.
+ *  - \p size must be less than or equal to the size of one symmetric block.
+ *  - \p socketfd must be a file descriptor for an already opened connection.
+ *  </p>
+ *
+ *  @param secret0 buffer of \p size bytes containing the first secret.
+ *  @param secret1 buffer of \p size bytes containing the first secret.
+ *  @param size number of bytes held in both \p secret0 and \p secret1.
+ *  @param socketfd file descriptor to the socket to use for the connection.
+ *  @return non-zero on failure.
+ */
+int OTsend(const unsigned char *secret0, const unsigned char *secret1,
+    size_t size, int socketfd)
+{
+}
+
+/**
+ *  @brief receive a given secret in an oblivious transfer.
+ *
+ *  Important parameter notes:
+ *  <p>
+ *  - \p size must be greater than or equal to the size of one symmetric block.
+ *  </p>
+ *
+ *  @param output buffer of size \p size to write the secret to.
+ *  @param size the number of bytes that \p output can hold.
+ *  @param which the secret number to receive: either secret 0 or secret 1.
+ *  @param socketfd file descriptor to the socket to use for the connection.
+ *  @return non-zero on failure.
+ */
+int OTreceive(unsigned char *output, size_t size, bool which, int socketfd)
+{
+}
