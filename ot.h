60228994696a9b774fe7605e709453aec796dcd9
void- <mr.alex.guy@gmail.com>
2014-11-02 10:15:51 -0800

    ot: Moved some constants to the header file.

--- ot.h
+++ ot.h
@@ -3,8 +3,10 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <openssl/aes.h>
 
 typedef uint16_t seq_t;
+#define SYM_SIZE AES_BLOCK_SIZE
 
 int OTsend(const unsigned char *, const unsigned char *, size_t, seq_t,
   int);
